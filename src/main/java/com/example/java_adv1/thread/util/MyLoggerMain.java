30c97edad479c609628106ec515d303c274df95b
ShaunAUS <wnsqud70@gmail.com>
2024-12-24 17:58:26 +0900

    add MyLogger

---
+++ src/main/java/com/example/java_adv1/thread/util/MyLoggerMain.java
@@ -0,0 +1,10 @@
+package com.example.java_adv1.thread.util;
+
+import static com.example.java_adv1.thread.util.MyLogger.*;
+
+public class MyLoggerMain {
+    public static void main(String[] args) {
+        log("test");
+        log(123123);
+    }
+}
